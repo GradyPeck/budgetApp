3da3bc056fc8dcd3a4be1593e604615c6fa508e3
Grady Peck <gradypeck717@gmail.com>
2019-12-12 19:13:53 -0500

    storing progress before changing approach

--- portionBar/portionBar.js
+++ portionBar/portionBar.js
@@ -1,9 +1,43 @@
-function resizePortion (percentage) {
-    let red = document.getElementById("red");
-    redWidth = 300 * (percentage/100);
-    redWidth = redWidth + "px";
-    red.style.width = redWidth;
+"use strict";
+
+class PortionBar {
+    constructor (element, size, ...portions) {
+        this.element = element;
+        this.size = size;
+        element.style.width = size + "px";
+        this.portions = {Aproperty: "blue"};
+        for(let i = 0; i < portions.size; i++) {
+            let portyElement = document.createElement("div");
+            let porty = new Portion (portions[i], portyElement);
+            this.portions[portions[i]] = porty;
+            console.log(portions[i]);
+        }
+        this.total = 0;
+    }
+
+    setTotal (number) {
+        this.total = number;
+    }
+
+    resizePortions (percentage) {
+        let totalUsed = 0;
+        for(let i = 0; i < this.portions.size; i++) {
+            let porty = this.portions[i];
+            porty.style[margin-left] = "100px";
+        }
+    }
 }
 
-//the argument you provide here is the percentage of the bar that Red will try to fill
-resizePortion(90);
+class Portion {
+    constructor (name, element) {
+        this.name = name;
+        this.element = element;
+    }
+}
+
+let babar = new PortionBar(document.getElementById("babar"), 300, "Food", "Owls");
+babar.setTotal(100);
+console.log(babar.portions);
+for(let porty in babar.portions) {
+    console.log(porty);
+}
