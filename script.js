7c61374d71acfe31bdd1a842ef7dc9e8c50e57fa
chase <chasebaker21@gmail.com>
2019-12-16 18:06:16 -0500

    added percentage mechanic

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 "use strict";
 
 function newBar() {
-    let a = document.getElementById("total").value;
-    let b = document.getElementById("spent").value;
-    let budget = "bills";
-    let spent = b;
-    let total = a;
-  
-    let x = `<li> <div
+  let total1 = document.getElementById("total1").value;
+  let spent1 = document.getElementById("spent1").value;
+  let total2 = document.getElementById("total2").value;
+  let spent2 = document.getElementById("spent2").value;
+
+  let budget1 = "bills";
+  let budget2 = "food";
+
+  let x = `<li> <div
       class="label-center"
-      id="${budget}"
+      id="${budget1}"
       
       ></div><div></div>
-      <h2>Entertainment</h2>
-      <p>${spent} of ${total} Spent</p>
+      <h2>Bills</h2>
+      <p>${spent1} of ${total1} Spent</p>
       </li>`;
-    document.getElementById("insertBudget").innerHTML = x;
-  
-    /* construct manually */
-    var bar1 = new ldBar("#bills", { preset: "fan" });
-  
-    // divides total by spent to create percentage amount below 100
-    // *********DOES NOT WORK FOR LARGE NUMBERS JAVASCRIPT ISSUE???*********
-    let c = a / b;
+  document.getElementById("insertBudget1").innerHTML = x;
+
+  let y = `<li> <div
+    class="label-center"
+    id="${budget2}"
     
-    /* ldBar stored in the element */
-    bar1.set(c);
-  }
-  
+    ></div><div></div>
+    <h2>Food</h2>
+    <p>${spent2} of ${total2} Spent</p>
+    </li>`;
+  document.getElementById("insertBudget2").innerHTML = y;
+
+  /* construct manually */
+  let bar1 = new ldBar("#bills", { preset: "bubble" });
+  let bar2 = new ldBar("#food", { preset: "bubble" });
+  // var bar2 = new ldBar("#entertainment", { preset: "bubble"});
+  // var bar3 = new ldBar("#food", { preset: "bubble"});
+  // var bar4 = new ldBar("#clothing", { preset: "bubble"});
+
+  // divides total by spent to create percentage amount below 100
+  let c = (spent1 / total1) * 100;
+  let d = (spent2 / total2) * 100;
+  /* ldBar stored in the element */
+  bar1.set(c);
+  bar2.set(d);
+  // bar3.set(c);
+  // bar4.set(c);
+}
