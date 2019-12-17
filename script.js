481fb1d60606b9d3510d04362c734d6cc0afea4e
chase <chasebaker21@gmail.com>
2019-12-16 19:24:56 -0500

    removed unneeded css

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 function newBar() {
   let total1 = document.getElementById("total1").value;
   let spent1 = document.getElementById("spent1").value;
-  let total2 = document.getElementById("total2").value;
-  let spent2 = document.getElementById("spent2").value;
+  // let total2 = document.getElementById("total2").value;
+  // let spent2 = document.getElementById("spent2").value;
 
 
   let budget1 = "bills";
-  let budget2 = "food";
+  // let budget2 = "food";
 
   let x = `<li> <div
       class="label-center"
@@ -20,29 +20,29 @@ function newBar() {
       </li>`;
   document.getElementById("insertBudget1").innerHTML = x;
 
-  let y = `<li> <div
-    class="label-center"
-    id="${budget2}"
+  // let y = `<li> <div
+  //   class="label-center"
+  //   id="${budget2}"
     
-    ></div><div></div>
-    <h2>Food</h2>
-    <p>${spent2} of ${total2} Spent</p>
-    </li>`;
-  document.getElementById("insertBudget2").innerHTML = y;
+  //   ></div><div></div>
+  //   <h2>Food</h2>
+  //   <p>${spent2} of ${total2} Spent</p>
+  //   </li>`;
+  // document.getElementById("insertBudget2").innerHTML = y;
 
   /* construct manually */
   let bar1 = new ldBar("#bills", { preset: "bubble" });
-  let bar2 = new ldBar("#food", { preset: "bubble" });
+  // let bar2 = new ldBar("#food", { preset: "bubble" });
   // var bar2 = new ldBar("#entertainment", { preset: "bubble"});
   // var bar3 = new ldBar("#food", { preset: "bubble"});
   // var bar4 = new ldBar("#clothing", { preset: "bubble"});
 
   // divides total by spent to create percentage amount below 100
   let c = (spent1 / total1) * 100;
-  let d = (spent2 / total2) * 100;
+  // let d = (spent2 / total2) * 100;
   /* ldBar stored in the element */
   bar1.set(c);
-  bar2.set(d);
+  // bar2.set(d);
   // bar3.set(c);
   // bar4.set(c);
 }
