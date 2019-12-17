4728e9c265bc405a1805e3aec7cdd7216e1b53de
Grady Peck <gradypeck717@gmail.com>
2019-12-16 20:36:51 -0500

    fully functioning portionbar

--- portionBar/portionBar.js
+++ portionBar/portionBar.js
@@ -1,32 +1,43 @@
 "use strict";
 
 let colors = ["red", "green", "blue", "orange", "violet", "yellow"];
+let sizes = {Food: 100, Flutes: 600, Coffee: 50, Owls: 150};
 
 class PortionBar {
-    constructor (element, size, ...portions) {
+    constructor (element, ...portions) {
+        //connect this object to its HTML element
         this.element = element;
-        this.size = size;
-        element.style.width = size + "px";
+        //set up empty portions object
         this.portions = {};
+        //iterate through array of category names
         for(let i = 0; i < portions.length; i++) {
+            //make new Portion object and element, and connect to this PortionBar
             let portyElement = document.createElement("div");
             let porty = new Portion (portions[i], portyElement);
             this.portions[portions[i]] = porty;
             this.element.appendChild(portyElement);
-            portyElement.classList.add("portion");
+            //style inner bars
             porty.setColor(colors[i]);
+            portyElement.style["height"] = getComputedStyle(this.element).height;
         }
         this.total = 0;
     }
 
+    //sets the total value
     setTotal (number) {
         this.total = number;
     }
 
+    //call this to resize the inner bars
     resizePortions () {
-        for(let i = 0; i < this.portions.length; i++) {
-            let porty = this.portions[i];
-            porty.style[width] = "100px";
+        let myWidth = getComputedStyle(this.element).width;
+        myWidth = Number(myWidth.slice(0, myWidth.length - 2));
+        let sizeFactor = myWidth/this.total;
+        console.log(sizeFactor);
+        for(let porty in this.portions) {
+            console.log(sizes[porty]);
+            console.log(sizes[porty]*sizeFactor);
+            this.portions[porty].element.style["width"] = sizes[porty]*sizeFactor + "px";
         }
     }
 }
@@ -42,6 +53,6 @@ class Portion {
     }
 }
 
-let babar = new PortionBar(document.getElementById("babar"), 300, "Food", "Owls", "Coffee");
-babar.setTotal(100);
-console.log(babar.portions);
+let babar = new PortionBar(document.getElementById("babar"), "Food", "Owls", "Coffee");
+babar.setTotal(500);
+babar.resizePortions();
